702e971934b77aab3e91fbbff4a987921862f0fa
Anton Matosov <anton.matosov@gmail.com>
2017-03-02 21:05:44 -0800

    More cleanup and eye opening that async works in node 7.6 natively

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import * as fs from "fs";
+// import * as fs from "fs";
 
 async function schrodinger() {
     return new Promise((resolve, reject) => {
@@ -21,3 +21,16 @@ async function ff () {
 }
 
 ff()
+
+// import http from 'http';
+
+let http = require('http')
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World 3\n');
+}).listen(1337, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:1337/');
+
+
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const ProgressPlugin = require("bitbar-webpack-progress-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel', // 'babel-loader' is also a valid name to reference
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new ProgressPlugin()
+  ]
+};
